a3ba9325711d0c81ec533bd04481dbc86537f271
Dominik Kapišinský <kapisinsky@microcomp.sk>
2015-02-23 14:44:57 +0100

    auth function for admin/tags

--- ckanext/edem/plugin.py
+++ ckanext/edem/plugin.py
@@ -348,6 +348,13 @@ def auth_comments_administration(context, data_dict=None):
         return {'success': True}
     return {'success': False, 'msg': _('Only moderator is authorized to manage comments and blogs.')}
 
+@logic.auth_allow_anonymous_access
+def auth_tags_administration(context, data_dict=None):
+    user_roles = user_custom_roles(context, data_dict)
+    if Roles.ROLE_DATA_CURATOR in user_roles:
+        return {'success': True}
+    return {'success': False, 'msg': _('Only data curator is authorized to manage tags of datasets.')}
+
 class EdemCustomPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IAuthFunctions)
     plugins.implements(plugins.IActions)
@@ -366,6 +373,7 @@ class EdemCustomPlugin(plugins.SingletonPlugin):
                 'app_edit' : auth_app_edit,
                 'app_editall' : auth_app_edit_all,
                 'storage_usage' : auth_storage_usage,
-                'commets_admin' : auth_comments_administration
+                'commets_admin' : auth_comments_administration,
+                'tags_admin' : auth_tags_administration
                 }
             
