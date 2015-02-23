87b60e067e80bc9f8f53b0784151efb57982b742
Dominik Kapišinský <kapisinsky@microcomp.sk>
2015-02-23 15:14:39 +0100

    updated sk locale

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ setup(
     license='GPL',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.edem'],
+    package_data={'': [
+        'i18n/*/LC_MESSAGES/*.po']},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
