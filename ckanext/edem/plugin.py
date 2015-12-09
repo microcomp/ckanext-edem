29c5ebc7bf36d6e786158a6d72db442c3c9ea31a
Dominik Kapišinský <kapisinsky@microcomp.sk>
2015-12-09 10:51:17 +0100

    db hack

--- ckanext/edem/plugin.py
+++ ckanext/edem/plugin.py
@@ -21,6 +21,27 @@ log = logging.getLogger(__name__)
 #from ckan.common import _
 _ = toolkit._
 
+### DB HACK
+from sqlalchemy import exc
+from sqlalchemy import event
+from sqlalchemy.pool import Pool
+
+@event.listens_for(Pool, "checkout")
+def ping_connection(dbapi_connection, connection_record, connection_proxy):
+    cursor = dbapi_connection.cursor()
+    try:
+        cursor.execute("SELECT 1")
+    except:
+        # optional - dispose the whole pool
+        # instead of invalidating one at a time
+        # connection_proxy._pool.dispose()
+
+        # raise DisconnectionError - pool will try
+        # connecting again up to three times before raising.
+        raise exc.DisconnectionError()
+    cursor.close()
+### DB END
+
 class Roles(object):
     MOD_R_PO = 'MOD-R-PO'
     MOD_R_DATA = 'MOD-R-DATA'
